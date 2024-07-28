1e779f6f535798381cda095f12bbc596e8b3b1c7
Nikolay Semyonov <nikolay.semyonov@exponea.com>
2024-07-29 00:46:19 +0200

    skip type checking on builds

--- next.config.js
+++ next.config.js
@@ -39,6 +39,13 @@ if (process.env.NODE_ENV === 'development') {
       // your project has ESLint errors.
       ignoreDuringBuilds: true,
     },
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
   };
 } else {
   module.exports = withPWA({
